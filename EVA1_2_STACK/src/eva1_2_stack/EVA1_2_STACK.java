85a211abc167de2135dac6b2ef8793dd6ef08178
1AldoPB <L25550357@chihuahua2.tecnm.mx>
2026-09-06 22:52:08 -0600

    Add files via upload

---
+++ EVA1_2_STACK/src/eva1_2_stack/EVA1_2_STACK.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_stack;
+
+/**
+ *
+ * @author aldop
+ */
+public class EVA1_2_STACK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("INICIA MAIN");
+        A();
+        System.out.println("TERMINA MAIN");
+    }
+    public static void A(){
+        System.out.println("INICA A");
+    B();
+        System.out.println("TERMINA A");
+    }
+    public static void B(){
+        System.out.println("INICA B");
+        int i=5;
+        double d=5.5;
+        System.out.println("TERMINA B");
+    }
+}
