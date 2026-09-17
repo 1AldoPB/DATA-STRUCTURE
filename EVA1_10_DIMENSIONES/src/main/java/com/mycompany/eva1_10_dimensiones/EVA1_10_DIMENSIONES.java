147e46fbe54024725f32c99f85f8fc37ff21a55a
1AldoPB <L25550357@chihuahua2.tecnm.mx>
2026-09-17 16:20:00 -0600

    Add files via upload

---
+++ EVA1_10_DIMENSIONES/src/main/java/com/mycompany/eva1_10_dimensiones/EVA1_10_DIMENSIONES.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_10_dimensiones;
+
+/**
+ *
+ * @author aldop
+ */
+public class EVA1_10_DIMENSIONES {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ EVA1_12_PRIMOS/src/eva1_12_primos/EVA1_12_PRIMOS.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_12_primos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aldop
+ */
+public class EVA1_12_PRIMOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Scanner sc= new Scanner(System.in);
+        for (double i = 0; i < i; i++) {
+         double resu1;
+       i=sc.nextDouble();
+       resu1= i/(i-1);
+            System.out.println("resultado "+ resu1);
+        }
+        for (double i = 0; i < i; i++) {
+            double resu2;
+            i=sc.nextDouble();
+            resu2=Math.sqrt(i);
+            if (resu) {
+                
+            } else {
+            }
+            
+        }
+    }
+    
+}
